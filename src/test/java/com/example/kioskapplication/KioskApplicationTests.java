be3bc5f96a4a7ab9017589abb6298a10a78ea74d
CLARENCE <clarence.ar9@gmail.com>
2025-09-24 23:18:11 +0800

    initial commit

---
+++ src/test/java/com/example/kioskapplication/KioskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.KioskApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
